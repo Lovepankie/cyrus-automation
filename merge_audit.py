2380090c29fbfb8c0fe6db9426c8f5f12b938408
Dennis Kaweesi <dkaweesi1@gmail.com>
2026-05-26 14:35:36 +0300

    security: move credentials to env var only

--- merge_audit.py
+++ merge_audit.py
@@ -18,51 +18,13 @@ import pandas as pd
 from google.oauth2.service_account import Credentials
 
 # ---------------------------------------------------------------------------
-# Service account — reads from env var in GitHub Actions, falls back to embedded
+# Service account — loaded from GOOGLE_SERVICE_ACCOUNT_JSON GitHub Secret
 # ---------------------------------------------------------------------------
 _ENV_SA = os.environ.get("GOOGLE_SERVICE_ACCOUNT_JSON")
-SERVICE_ACCOUNT_INFO = json.loads(_ENV_SA) if _ENV_SA else {
-    "type": "service_account",
-    "project_id": "drive-399813",
-    "private_key_id": "be807337b7d6b3391199a4421ffbb1739bf7430f",
-    "private_key": (
-        "-----BEGIN PRIVATE KEY-----\n"
-        "MIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDhyt1lrbccnrqE\n"
-        "Sb+o/6do/I1p/MvrdjP/ZZBgTgn7765uE13rWrKc/bkUBmEwyCTVXGOuB4bTCmsx\n"
-        "z/NuU+Qea/0cT339zGCNj2c9Fam3HIneL9e/vSC0yj8KAjFNBbzzRr1BJL8S+pML\n"
-        "lMouH9QqJWsW5+ldakSV/g8bxk52INitXmqTizC9aonrMfXIK0C9+uYk0HFesWX1\n"
-        "JxfG2PeZ8s4yUpxDFbDGtUjRI5Xpv2GXo9ywjRzm52EuRQ6PVmAcbyEeybG9Zhix\n"
-        "+z0K5hAVSnvJl7WTY4xCLNYEmk32E8pBFceUwITWfsyLHiNtMxjLAJqyFZCP3a31\n"
-        "mMyPbgJRAgMBAAECggEACIGhB6ScwmpEcplorBA9mVnyZezNLei9GsETyF0ISUKF\n"
-        "WsZGAojfM8TnRRbccH8JzDP32WWVMbwxv6Wq8Rwd+vtshLWF9JrPhGXDqOx5AVoE\n"
-        "46b/xMx2P/limJjujI1Lygp/NMYSoL9p7MTFPSmbz56rQyinhQps3Q2+OfilmhOe\n"
-        "lW744GYZz4DgFqEwEWQN/JYU4K4Rp2DMZ1Vxou5RhRMFKDg/QVU9yfdWwfAIb56x\n"
-        "Nsj+fYI8bXCAC+wGLAgSSBndQzAV96MyMx//PhnWGOWu1vkcEDb6rfhYVwMKB7/n\n"
-        "FDhkr9j3pGles2Sr2JR3tQx5pllFVcui/P6vOOeshQKBgQD5fPPzV1Pj/178+QVB\n"
-        "4VV4sf5eZPSXijjRMaWsfDsr3glJkGhtrbXYv4U8IBCzFwght/TuYZ9hk1EBJV9/\n"
-        "mp/pQ5L9B74+X5hpFK/9ont6+snhBqmbX/EF8+DBn/XJVn3yxlSGutQHcpCwuBZq\n"
-        "hRwKcCNhTnsW8i+v7gkmVYd9YwKBgQDnr5SiOe/xOpqt3Z3AYz7GoeitnpTKpT9D\n"
-        "+pQV61V67BHueICUp109NCyNi1qZCO4d0LsjIslyx7kPOEYUf1C3Ul1LsUr+cAkC\n"
-        "1zvdNRVvmNB4RQ+WzCwHgOSsQXQyNxO74uj5fZNffVT0CJjbgvbHsQIQWQkj9vS6\n"
-        "dvQZJwpZuwKBgQC7jQ6LCUQcataKj90+6FlrkUsqxPQGk7cgtBTatM16rcEHl1KZ\n"
-        "2POSTG+pgmVrbE4FoxeyuJqrLKbBmMnQ0HmUTuNJ6i8/DngxzoZ6wlHXYn6u9mY8\n"
-        "UoSLOAnnJQwNXkLyZdwXKI3KR3q0Dr9zNjudMS23sdrgphHpTKV+Nt/TBwKBgHSG\n"
-        "6NjXDwljEl0UM72JrQ82a9K3CdsKVdGY/FYx8OJMTZCFZxZdPxYDYc0nI8AIr8qr\n"
-        "KxQ28N8b+MXg5c51YmFxuZ7SYwepzb5yBpfxlQB1+ZQkF/0eX56+g0Tn/ssqzHAZ\n"
-        "ZlflgvPqE4pRsJ/nNLunGYSjY2eFU/1cytTDv/71AoGACmCAabda8YQzHwovE9ar\n"
-        "4K5GT6m7L6T4357FtnacoE17j9YcMJrZNeTaddYshIL/8HzpFJtZQXxmTk/XfVpm\n"
-        "DyxCj1BUjH5H4MjnDc6efS5Jz5CkljEiWPupTAMRyOg+LV085j+sg/zvZSewfAdw\n"
-        "o80f1uB4a2fQb77Kw4YLc+E=\n"
-        "-----END PRIVATE KEY-----\n"
-    ),
-    "client_email": "download-sheets@drive-399813.iam.gserviceaccount.com",
-    "client_id": "110748723405822534910",
-    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-    "token_uri": "https://oauth2.googleapis.com/token",
-    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-    "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/download-sheets%40drive-399813.iam.gserviceaccount.com",
-    "universe_domain": "googleapis.com",
-}
+if not _ENV_SA:
+    print("ERROR: GOOGLE_SERVICE_ACCOUNT_JSON environment variable is not set.")
+    sys.exit(1)
+SERVICE_ACCOUNT_INFO = json.loads(_ENV_SA)
 
 # ---------------------------------------------------------------------------
 # Region sheets
